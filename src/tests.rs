db9712c06adea57c0d86d2255e9be9169ca3795f
whisperpine <yusong.lai@icloud.com>
2023-04-13 23:10:55 +0800

    rename repository

--- src/tests.rs
+++ src/tests.rs
@@ -1,6 +1,7 @@
-#![allow(unused_imports)]
+// #![allow(unused_imports)]
 
 use super::*;
+// use anyhow::Result;
 
 #[test]
 fn demo_test() {
@@ -9,12 +10,27 @@ fn demo_test() {
 
 #[test]
 fn ahash_test() {
-    use ahash::{AHashMap, RandomState};
-    use std::collections::HashMap;
-
-    let mut map: HashMap<i32, i32, RandomState> = HashMap::default();
-    map.insert(12, 34);
-
+    use ahash::AHashMap;
     let mut nice: AHashMap<i32, i32> = AHashMap::new();
     nice.insert(1234, 3);
+}
+
+#[test]
+fn buf_lock_write() -> Result<()> {
+    use std::fs::File;
+    use std::io::{BufRead, BufReader, BufWriter, Write};
+
+    let output_file = File::create("yahaha.txt")?;
+    let mut output = BufWriter::new(output_file);
+
+    let file = File::open("nice.txt")?;
+    let mut lock = std::io::stdout().lock();
+    for line in BufReader::new(file).lines() {
+        let text = line?;
+        writeln!(lock, "{}", text)?;
+        writeln!(output, "{}", text)?;
+    }
+    output.flush()?;
+
+    Ok(())
 }
