7f7ef234a716dc1e366ba73b368abdd860885a5f
whisperpine <yusong.lai@icloud.com>
2023-11-17 22:38:03 +0800

    fix an issue that prevented the process to receive SIGTERM

--- src/lib.rs
+++ src/lib.rs
@@ -31,8 +31,31 @@ use uuid::Uuid;
 ///
 /// Shutdown the server when pressing Ctrl+C.
 pub async fn shutdown() {
-    tokio::signal::ctrl_c().await.unwrap();
-    info!("shutdown");
+    use tokio::signal;
+
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+
+    tracing::info!("starting graceful shutdown");
 }
 
 /// Routing fallback
